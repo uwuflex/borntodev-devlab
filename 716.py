1ba03d2f402bfca850a18ee994cd3d3670e3d093
uwuflex <45969692+uwuflex@users.noreply.github.com>
2022-07-03 11:15:15 +0700

    Update 716.py

--- 716.py
+++ 716.py
@@ -1,3 +1,5 @@
+## https://www.borntodev.com/devlab/task/716 ##
+
 for i in range(int(input())):
     list = input().split(", ")
     print( ( (-1)**(len(list)+1)) * (int(list[0])) * (int(list[len(list)-1] )))
